95a9895577d6fe65a7cb216c530bdf596162b788
BananabroJR <emiller@student.neumont.edu>
2023-02-13 18:30:59 -0700

    Spymaster Transsition

--- PRO150/Controllers/HomeController.cs
+++ PRO150/Controllers/HomeController.cs
@@ -47,6 +47,11 @@ namespace PRO150.Controllers
             return View();
         }
 
+        public IActionResult SpymasterTran()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
