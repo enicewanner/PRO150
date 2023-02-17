0704d76ad96967c409d308203f77c69f2dd7af6e
BananabroJR <emiller@student.neumont.edu>
2023-02-16 18:48:04 -0700

    Made it so only one assassin shows up

--- PRO150/Models/Card.cs
+++ PRO150/Models/Card.cs
@@ -4,8 +4,15 @@
     {
         public static int[] CardID = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
 
-        public static string[] colors = { "Black", "Red", "Blue", "White" };
+        public static string[] colors = { "Red", "Blue", "White", "Black" };
+
+        public static bool isAssassin = false;
+        public static int whiteColorCount = 0;
+        public static int blueColorCount = 0;
+        public static int redColorCount = 0;
 
         public static Random rand = new Random();
+
+        
     }
 }
